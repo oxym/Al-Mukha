38ff2947609da11f124417b9c77a48a3a6304022
Farzam Noori <farzamn80@gmail.com>
2018-03-22 13:28:11 -0600

    added register page and styling, currently unfinished

--- index.php
+++ index.php
@@ -27,31 +27,27 @@
 		<div class="collapse navbar-collapse" id="navbarSupportedContent" action="search.php" method="POST">
 			<ul class="navbar-nav mr-auto">
 
-				<form class="form-inline my-2 my-lg-0">
-					<input class="form-control mr-sm-2 search-bar" name="pname" type="text" placeholder="Search" aria-label="Search">
-					<select name="type" class="mr-sm-2 custom-select search-select">
-						<option value="product">Product</option>
-						<option value="store">Store</option>
-						<option value="owner">Owner</option>
-					</select>
-					<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-				</form>
+				<li>
+					<form class="form-inline my-2 my-lg-0" action="search.php" method="POST">
+						<input class="form-control mr-sm-2 search-bar" name="pname" type="text" placeholder="Search" aria-label="Search">
+						<select name="type" class="mr-sm-2 custom-select search-select">
+							<option value="product">Product</option>
+							<option value="store">Store</option>
+							<option value="owner">Owner</option>
+						</select>
+						<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+					</form>
+				</li>
 
+				<li class="register-button mr-2">
+					<a class="nav-link" href="register.php">Register |</a>
+				</li>
+				<li class="login-button mr-2">
+					<a class="nav-link" href="login.php">Login</a>
+				</li>
 			</ul>
+
 		</div>
 	</nav>
-
-<!--
-<form action="search.php" method="POST">
-	<input type="text" name="pname">
-	<select name="type">
-		<option value="product">Product</option>
-		<option value="store">Store</option>
-		<option value="owner">Owner</option>
-	</select>
-
-	<button type="submit">Search</button>
-</form>
--->
 </body>
 </html>
---
+++ login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Login</title>
+
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<!-- Popper JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+	<!-- main page stylesheet -->
+	<link rel="stylesheet" href="./css/login.css">
+
+</head>
+<body>
+
+	<form class="login-details" action="search.php" method="POST">
+		<div class="mb-4">
+			Welcome To Al-Mukha
+		</div>
+
+		<input class="form-control mr-sm-2 mb-2 search-bar" name="email" type="email" placeholder="Email" aria-label="Email">
+		<input class="form-control mr-sm-2 mb-4 search-bar" name="password" type="password" placeholder="Password" aria-label="Password">
+
+		<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+
+	</form>
+
+</body>
+</html>
