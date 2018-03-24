3449d972ba56feb407cc2027ff5ac0b754318427
Farzam Noori <farzamn80@gmail.com>
2018-03-24 13:08:11 -0600

    Added the main page content, where the tea and coffees will be displayed

--- index.php
+++ index.php
@@ -53,56 +53,65 @@
 	<div class="main-page-content">
 		<div class="card">
 			<div class="card-title">
-				<div id="productTitle">Product Title</div>
+				<div id="productTitle">
+					<div id="productType">(Tea/Coffee)</div>
+					Product Title
+				</div>
 				<div id="productPrice" class="col-md-4">$14.00</div>
 			</div>
-			<div class="row productInfo">
-				<div class="col-md-6">
-					<div>General Information</div>
 
-					<div class="col-md-4">
-						<div>Stock</div>
-						<div id="productStock">4</div>
-					</div>
-					
-					<div class="col-md-4">
-						<div>Origin</div>
-						<div id="productOrigin">Mexico</div>
-					</div>
+			<div class="row card-content">
+				<ul class="list-group col-md-6">
+					<li class="list-group-item product-detail-row">
+						<div class="product-detail-row-title">Stock</div>
+						<div id="stockVal" class="product-detail-row-value">123</div>
+					</li>
+					<li class="list-group-item">
+						<div class="product-detail-row-title">Origin</div>
+						<div id="originVal" class="product-detail-row-value">Venezuela</div>
+					</li>
+					<li class="list-group-item">
+						<div class="product-detail-row-title">Expiry Date</div>
+						<div id="expiryDateVal" class="product-detail-row-value">May 9, 2018</div>
+					</li>
+					<li class="list-group-item">
+						<div class="product-detail-row-title">Shelve Date</div>
+						<div id="shelveDateVal" class="product-detail-row-value">April 9, 2018</div>
+					</li>
+					<li class="list-group-item">
+						<div class="product-detail-row-title">Store Name</div>
+						<div id="storeName" class="product-detail-row-value">Best Coffee Shop</div>
+					</li>
+					<li class="list-group-item">
+						<div class="product-detail-row-title">Salesperson</div>
+						<div id="salesperson" class="product-detail-row-value">John Doe</div>
+					</li>
+				</ul>
 
-					<div class="col-md-4">
-						<div>Expiry date</div>
-						<div id="productExpiryDate">April 2019</div>
-					</div>
-
-					<div class="col-md-4">
-						<div>Shelve date</div>
-						<div id="productShelveDate">March 2019</div>
-					</div>
-
-				</div>
-
-				<div class="col-md-6">
-					<div>Specific Information</div>
-
-					<div class="col-md-4">
-						<div>Bean Type</div>
-						<div id="beanType">Good Bean</div>
-					</div>
-					
-					<div class="col-md-4">
-						<div>Roast Type</div>	
-						<div id="roastType">Best Roast</div>
-					</div>
-
-					<div class="col-md-4">
-						<div>Roast Date</div>
-						<div id="roastDate">March 2019</div>
-					</div>
-				</div>
-
-
+				<ul class="list-group col-md-6">
+					<li id="beanType" class="list-group-item">
+						<div class="product-detail-row-title">Bean Type</div>
+						<div id="beanTypeVal" class="product-detail-row-value">Large</div>
+					</li>
+					<li id="roastType" class="list-group-item">
+						<div class="product-detail-row-title">Roast Type</div>
+						<div id="roastTypeVal" class="product-detail-row-value">Light</div>
+					</li>
+						<li id="teaType" class="list-group-item">
+						<div class="product-detail-row-title">Tea Type</div>
+						<div id="teaTypeVal" class="product-detail-row-value">Green</div>
+					</li>
+					<li id="teaGrade" class="list-group-item">
+						<div class="product-detail-row-title">Tea Grade</div>
+						<div id="teaGradeVal" class="product-detail-row-value">AAA</div>
+					</li>
+					<li id="roastDate" class="list-group-item">
+						<div class="product-detail-row-title">Roast Date</div>
+						<div id="roastDateVal" class="product-detail-row-value">April 2, 2018</div>
+					</li>
+				</ul>
 			</div>
+
 		</div>	
 	</div>
 
