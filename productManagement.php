2bae7eb8e93c2da4c77f2a66455d19ed1f1af936
Farzam Noori <farzamn80@gmail.com>
2018-03-24 19:12:00 -0600

    Added a logout button for the product management page

--- productManagement.php
+++ productManagement.php
@@ -23,8 +23,11 @@
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
 		</button>
-		<div class="login-button mr-2">
+		<div class="productManagementPageOwnerInfo mr-2">
 			Owner/Salesperson name
+		</div>
+		<div class="logout-button mr-2">
+			<a href="index.php">Logout</a>
 		</div>
 	</nav>
 
