845a85fb4b35b1d91106378774e71cfab5f42d64
Michael Han <32470787+oxym@users.noreply.github.com>
2018-04-03 23:23:49 -0500

    Delete search.php

--- includes/search.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include 'includes/search.inc.php';
-
-$pname = $_POST['pname'];
-$searchType = $_POST['type']
-
-$search = New Search($pname,$searchType);
-$msg = $search->searchByPartialName();
-echo $msg;
